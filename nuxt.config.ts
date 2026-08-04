462aa795eaf4379e3fe4aec725704cd9d087e6ba
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-04 17:18:22 +0500

    install axios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: false }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: false },
+  css: ["~/shared/styles/reset.css", "~/shared/styles/global.css"],
+});
