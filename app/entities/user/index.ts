40bc8a904e962b6ea972f6c9d5f524d6f408656d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-08-04 17:14:43 +0500

    feat(entities/user): add User type and CRUD api

---
+++ app/entities/user/index.ts
@@ -0,0 +1,5 @@
+// model
+export type { IUser } from "./model/types";
+
+// ui
+export { default as UserAvatar } from "./ui/userAvatar.vue";
---
+++ app/entities/user/ui/userAvatar.vue
@@ -0,0 +1,5 @@
+<template>
+    user avatar
+</template>
+
+<script setup lang="ts"></script>
